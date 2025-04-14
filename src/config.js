445d3d498f8850bc138b88f7fe456f6304969b26
opengithubcode <bqveum6r5@relay.firefox.com>
2025-04-14 13:58:50 +0530

    Added Manrope font support and screensaver customization

--- src/config.js
+++ src/config.js
@@ -2,11 +2,8 @@ const CONFIG_KEY = 'ytaf-configuration';
 
 const configOptions = new Map([
   ['enableAdBlock', { default: true, desc: 'Enable ad blocking' }],
-  ['upgradeThumbnails', { default: false, desc: 'Upgrade thumbnail quality' }],
-  [
-    'removeShorts',
-    { default: false, desc: 'Remove Shorts from subscriptions' }
-  ],
+  ['upgradeThumbnails', { default: true, desc: 'Upgrade thumbnail quality' }],
+  ['removeShorts', { default: true, desc: 'Remove Shorts from subscriptions' }],
   ['enableSponsorBlock', { default: true, desc: 'Enable SponsorBlock' }],
   [
     'enableSponsorBlockSponsor',
